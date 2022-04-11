717dc4c389ce4522abd8ed2103676ecfbe4095ed
unknown <um_2005@mail.ru>
2022-04-11 12:09:01 +0500

    text commit 2

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,1 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('layout');
-});
-
-Route::resource('lots', \App\Http\Controllers\WebSiteControllers\LotController::class)
-    ->except(['show']);
-
-Route::resource('files', \App\Http\Controllers\WebSiteControllers\FileController::class)
-    ->except(['show']);
-
-Route::get('/getfile/{id}', [\App\Http\Controllers\WebSiteControllers\FileController::class, 'getFile'])->name('getFile');
-
-Route::resource('filetype', \App\Http\Controllers\WebSiteControllers\FileTypesController::class);
+<?phpuse Illuminate\Support\Facades\Route;Route::get('/', function () {    return view('layout');});Route::resource('lots', \App\Http\Controllers\WebSiteControllers\LotController::class)    ->except(['show']);Route::resource('files', \App\Http\Controllers\WebSiteControllers\FileController::class)    ->except(['show']);Route::get('/getfile/{id}', [\App\Http\Controllers\WebSiteControllers\FileController::class, 'getFile'])->name('getFile');Route::resource('filetype', \App\Http\Controllers\WebSiteControllers\FileTypesController::class);
